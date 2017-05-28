4a5802c32eefcdb7cd4f6bffa029acf270772d98
UAdrian <vladia_adrian@hotmail.com>
2017-05-28 17:16:17 +0300

    UI v1.5

--- src/application/DashboardController.java
+++ src/application/DashboardController.java
@@ -1,26 +1,49 @@
 package application;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-public class DashboardController {
+public class DashboardController implements Initializable {
   @FXML
   private Label userLbl;
+  @FXML
+  private BorderPane accInfo;
+  @FXML
+  private Stage window;
 
   public void logOut(ActionEvent event) throws Exception {
     ((Node) event.getSource()).getScene().getWindow().hide();
-    Stage primaryStage = new Stage();
-    FXMLLoader loader = new FXMLLoader();
-    Pane root = loader.load(getClass().getResource("/application/Login.fxml").openStream());
+    window = new Stage();
+    Pane root = FXMLLoader.load(getClass().getResource("/application/Login.fxml"));
     Scene scene = new Scene(root);
     scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-    primaryStage.setScene(scene);
-    primaryStage.show();
+    window.setScene(scene);
+    window.show();
   }
+
+  public void goAccountInfo(ActionEvent event) throws Exception {
+    accInfo = (BorderPane) FXMLLoader.load(getClass().getResource("/application/Dashboard.fxml"));
+    Pane center = (Pane) FXMLLoader.load(getClass().getResource("/application/AccInfo.fxml"));
+    accInfo.setPadding(new Insets(5, 5, 5, 5));
+    accInfo.setCenter(center);
+    ((Node) event.getSource()).getScene().setRoot(accInfo);
+  }
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    userLbl.setText("Welcome user!");
+  }
+
 }
--- src/application/LoginController.java
+++ src/application/LoginController.java
@@ -3,17 +3,16 @@ package application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 
 public class LoginController {
-  @FXML
-  private Label userLbl;
-
   @FXML
   private Label lblStatus;
 
@@ -24,10 +23,11 @@ public class LoginController {
   private TextField txtPassword;
 
   public void login(ActionEvent event) throws Exception {
-    if (txtUsername.getText().equals("user") && txtPassword.getText().equals("user")) {
+    if (txtUsername.getText().equals("") && txtPassword.getText().equals("")) {
       ((Node) event.getSource()).getScene().getWindow().hide();
       Stage primaryStage = new Stage();
       Parent root = FXMLLoader.load(getClass().getResource("/application/Dashboard.fxml"));
+      ((Region) root).setPadding(new Insets(5, 5, 5, 5));
       Scene scene = new Scene(root);
       scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
       primaryStage.setTitle("Dashboard");
