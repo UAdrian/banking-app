d6fd1da87776496e43721f28ac6131147f58f57e
UAdrian <vladia_adrian@hotmail.com>
2017-05-29 10:53:34 +0300

    History

--- src/application/HistoryController.java
+++ src/application/HistoryController.java
@@ -1,5 +1,33 @@
 package application;
 
-public class HistoryController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import classes.History;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class HistoryController implements Initializable {
+  @FXML
+  private TableView<History> table;
+  @FXML
+  private TableColumn<History, String> data;
+  @FXML
+  private TableColumn<History, String> details;
+
+  public ObservableList<History> list = FXCollections.observableArrayList(new History("27.02.2017", "1250 RON"),
+      new History("27.02.2017", "125 RON"), new History("27.02.2017", "130 RON"), new History("27.02.2017", "130 RON"));
+
+  @Override
+  public void initialize(URL arg0, ResourceBundle arg1) {
+    data.setCellValueFactory(new PropertyValueFactory<History, String>("date"));
+    details.setCellValueFactory(new PropertyValueFactory<History, String>("details"));
+    table.setItems(list);
+  }
 
 }
---
+++ src/classes/History.java
@@ -0,0 +1,21 @@
+package classes;
+
+public class History {
+  private String date;
+  private String details;
+
+  public History(String date, String details) {
+    super();
+    this.date = date;
+    this.details = details;
+  }
+
+  public String getDate() {
+    return date;
+  }
+
+  public String getDetails() {
+    return details;
+  }
+
+}
